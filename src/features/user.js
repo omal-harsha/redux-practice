e7199b2ace64d54f05d1cb2f3e416ed55b00ea29
Omal Harsha <omalharshanuwan@gmail.com>
2023-07-20 15:42:13 +0530

    completed code

---
+++ src/features/user.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialStateValue = { value: { name: "", age: 0, email: ""}}
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: initialStateValue,
+    reducers: {
+        login: (state,action) => {
+            state.value = action.payload;
+        },
+        logout: (state) => {
+            state.value = initialStateValue
+        }
+    }
+})
+
+export const {login, logout} = userSlice.actions;
+export default userSlice.reducer;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {configureStore} from "@reduxjs/toolkit"
+import {Provider} from "react-redux"
+import useReducer from './features/user';
+import themeReducer from './features/theme';
+
+
+const store = configureStore({
+  reducer: {  
+    user: useReducer,
+    theme: themeReducer,
+  },
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
